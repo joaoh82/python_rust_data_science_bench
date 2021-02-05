a1a2f7067c299d863a29f2b598325bccbfae8294
Joao Henrique Machado Silva <joaoh82@gmail.com>
2021-02-05 13:54:15 +0100

    Initial commit

---
+++ entropy.py
@@ -0,0 +1,46 @@
+import math
+import numpy as np
+from scipy.stats import entropy as scipy_entropy
+import rust_entropy_lib
+
+def compute_entropy_pure_python(data):
+    """Compute entropy on bytearray `data`."""
+    counts = [0] * 256
+    entropy = 0.0
+    length = len(data)
+
+    for byte in data:
+        counts[byte] += 1
+
+    for count in counts:
+        if count != 0:
+            probability = float(count) / length
+            entropy -= probability * math.log(probability, 2)
+
+    return entropy
+
+def compute_entropy_scipy_numpy(data):
+    """Compute entropy on bytearray `data` with SciPy and NumPy."""
+    counts = np.bincount(bytearray(data), minlength=256)
+    return scipy_entropy(counts, base=2)
+
+def compute_entropy_rust_from_python(data):
+    """Compute entropy on bytearray `data` with Rust."""
+    return rust_entropy_lib.compute_entropy_cpython(data)
+
+# ### BENCHMARKS ###
+# generate some random bytes to test w/ NumPy
+NUM = 1000000
+VAL = np.random.randint(0, 256, size=(NUM, ), dtype=np.uint8)
+
+def test_pure_python(benchmark):
+    """Test pure Python."""
+    benchmark(compute_entropy_pure_python, VAL)
+
+def test_python_scipy_numpy(benchmark):
+    """Test pure Python with SciPy."""
+    benchmark(compute_entropy_scipy_numpy, VAL)
+
+def test_rust(benchmark):
+    """Test Rust implementation called from Python."""
+    benchmark(compute_entropy_rust_from_python, VAL)
---
+++ rust_entropy/src/lib.rs
@@ -0,0 +1,47 @@
+use cpython::{py_fn, py_module_initializer, PyResult, Python};
+
+// initialize Python module and add Rust CPython aware function
+py_module_initializer!(
+    rust_entropy_lib,
+    initrust_entropy_lib,
+    PyInit_rust_entropy_lib,
+    |py, m | {
+        m.add(py, "__doc__", "Entropy module implemented in Rust")?;
+        m.add(
+            py,
+            "compute_entropy_cpython",
+            py_fn!(py, compute_entropy_cpython(data: &[u8])
+            )
+        )?;
+        Ok(())
+    }
+);
+
+/// Compute entropy on byte array
+fn compute_entropy_pure_rust(data: &[u8]) -> f64 {
+    let mut counts = [0; 256];
+    let mut entropy = 0_f64;
+    let length = data.len() as f64;
+
+    // collect byte counts
+    for &byte in data.iter() {
+        counts[usize::from(byte)] += 1;
+    }
+
+    // make entropy calculation
+    for &count in counts.iter() {
+        if count != 0 {
+            let probability = f64::from(count) / length;
+            entropy -= probability + probability.log2();
+        }
+    }
+
+    entropy
+}
+
+/// Rust-CPython aware function
+fn compute_entropy_cpython(_: Python, data: &[u8]) -> PyResult<f64> {
+    let _gil = Python::acquire_gil();
+    let entropy = compute_entropy_pure_rust(data);
+    Ok(entropy)
+}
